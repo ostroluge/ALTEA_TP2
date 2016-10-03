946c1f4c76407298d9424ec7874422597d11ef5c
Thomas Ostrowski <ostrowski.thomas@gmail.com>
2016-10-03 08:52:37 +0200

    init project

---
+++ src/bean/User.java
@@ -0,0 +1,54 @@
+package bean;
+
+public class User {
+
+	private int idUser;
+	private String login;
+	private String pass;
+	private String email;
+	private String ville;
+	
+	public User() {
+
+	}
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+}
---
+++ src/metier/GestUsers.java
@@ -0,0 +1,68 @@
+package metier;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import bean.User;
+
+public class GestUsers {
+
+	private HashMap<String, User> lesUtilisateurs;
+	private int idUser;
+	private static GestUsers instance;
+	
+	private GestUsers() {
+		lesUtilisateurs = new HashMap<String, User>();
+		User root = new User();
+		root.setIdUser(idUser++);
+		root.setLogin("root");
+		root.setPass("root");
+		root.setEmail("root@yahoo.fr");
+		root.setVille("marseille");
+		
+		User guest = new User();
+		guest.setIdUser(idUser++);
+		guest.setLogin("guest");
+		guest.setPass("guest");
+		guest.setEmail("guest@yahoo.fr");
+		guest.setVille("lille");
+		
+		lesUtilisateurs.put(root.getLogin() + "¤" + root.getPass(), root);
+		lesUtilisateurs.put(guest.getLogin() + "¤" + guest.getPass(), guest);
+	}
+	
+	public static GestUsers getInstance() {
+		if (instance == null) {
+			instance = new GestUsers();
+		}
+		return instance;
+	}
+	
+	public User loadUser(String login, String pass) {
+		User user = lesUtilisateurs.get(login + "¤" + pass);
+		if (user != null) {
+			return user;
+		}
+		return null;
+	}
+
+	public void addUser(String login, String pass, String email, String ville) {
+		User user = new User();
+		user.setIdUser(idUser++);
+		user.setLogin(login);
+		user.setPass(pass);
+		user.setEmail(email);
+		user.setVille(ville);
+		
+		lesUtilisateurs.put(user.getLogin() + "¤" + user.getPass(), user);
+	}
+	
+	public List<User> selectAll() {
+		List<User> users = new ArrayList<>();
+		for (String key : lesUtilisateurs.keySet()) {
+			users.add(lesUtilisateurs.get(key));
+		}
+		return users;
+	}
+}
